378646bd4cc9f89b287c55dd116bd614da8eeb0c
chuwx <var_chx@126.com>
2020-01-01 21:43:40 +0800

    项目中引入antd 测试成功

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
+import { message, Button } from 'antd'
+
 export default class App extends Component {
+    
     render() {
+        const success = () => {
+            message.success('This is a success message');
+          }
         return (
             <div>
-                起步了
+                <Button onClick={success}>Success</Button>
             </div>
         )
     }
