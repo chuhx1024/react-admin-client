c59583db77407c2e8f1a570425bff0afc8690e2e
chuwx <var_chx@126.com>
2020-01-05 21:53:37 +0800

    优化 时间格式化函数

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,41 +1,34 @@
 
 export const formateDate = (time, separator = '-', type) => {
-  if (!time && typeof (time) !== 'number') {
-      return ''
-  }
-  // 后台返回的时间戳可能是字符串类型的可能是几个时间戳
   let tempArray = []
-  let date
-  if (time instanceof Date) {} else {
-      time = time && parseInt(time)
-  }
-  date = new Date(time)
-  let year = date.getFullYear()
-  let month = date.getMonth() < 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
-  let day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
-  let hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
-  let min = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
-  let sec = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
+  // 定义补0 函数
+  let tf = (i) => i< 10 ? `0${i}` : i
+  let date = new Date(time)
+  let yea = date.getFullYear()
+  let mon = tf(date.getMonth() + 1)
+  let day = tf(date.getDate())
+  let hou = tf(date.getHours())
+  let min = tf(date.getMinutes())
+  let sec = tf(date.getSeconds()) 
   switch (type) {
       case 'YY-MM-DD':
-        tempArray = [year, month, day]
+        tempArray = [yea, mon, day]
         break
       case 'YY-MM':
-        tempArray = [year, month]
+        tempArray = [yea, mon]
         break
       case 'MM-DD':
-        tempArray = [month, day]
+        tempArray = [mon, day]
         break
       case 'hh-mm-ss':
-        tempArray = [hour, min, sec]
+        tempArray = [hou, min, sec]
         break
       case 'hh-mm':
-        tempArray = [hour, min]
+        tempArray = [hou, min]
         break
       default:
-        let temp = day + '\xa0\xa0\xa0' + [hour, min, sec].join(':')
-        tempArray = [year, month, temp]
-        
+        let temp = day + '\xa0\xa0\xa0' + [hou, min, sec].join(':')
+        tempArray = [yea, mon, temp]
   }
   return tempArray.join(separator)
 }
