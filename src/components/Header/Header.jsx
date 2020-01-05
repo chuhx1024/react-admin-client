ad76f81cbeed2775bdba5b91efdf06bfa441c256
chuwx <var_chx@126.com>
2020-01-05 20:48:48 +0800

    header的动态效果实现

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,37 @@ import React, { Component } from 'react'
 import './Header.less'
 import { Button } from 'antd'
 import { reqWeather } from '../../api/index'
+import { formateDate } from '../../utils/dateUtils'
 
 
 
 export default class Header extends Component {
+  state = {
+    weather: '',
+    dayPictureUrl: '',
+    time: formateDate(Date.now())
+  }
   // 获取天气的数据
   getWeather = async () => {
-    const res = await reqWeather('北京')
-    console.log(res)
+    const { weather, dayPictureUrl } = await reqWeather('北京')
+    this.setState({weather, dayPictureUrl})
+  }
+  // 创建时间的定时器
+  getDate = () => {
+    setInterval(() => {
+      this.setState({
+        time: formateDate(Date.now())
+      })
+    }, 1000)
+
   }
   componentDidMount () {
     this.getWeather()
+    this.getDate()
   }
   render() {
-    return (
+    const {weather, dayPictureUrl, time} = this.state
+    return ( 
       <div className="header">
         <div className="header-top">
           <span>欢迎, admin</span>
@@ -24,7 +41,9 @@ export default class Header extends Component {
         <div className="header-bottom">
           <div className="header-bottom-left">首页</div>
           <div className="header-bottom-right">
-            <span>2020-05-29</span>
+            <span>{time}</span>
+            <img src={dayPictureUrl} alt=""/>
+            <span>{weather}</span>
           </div>
         </div>
       </div>
