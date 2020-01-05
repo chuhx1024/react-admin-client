dbf335f9ca46ca4494fcf362b3c8291c711ae8f6
chuwx <var_chx@126.com>
2020-01-05 23:00:27 +0800

    格式化代码

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -11,24 +11,24 @@ export const formateDate = (time, separator = '-', type) => {
   let min = tf(date.getMinutes())
   let sec = tf(date.getSeconds()) 
   switch (type) {
-      case 'YY-MM-DD':
-        tempArray = [yea, mon, day]
-        break
-      case 'YY-MM':
-        tempArray = [yea, mon]
-        break
-      case 'MM-DD':
-        tempArray = [mon, day]
-        break
-      case 'hh-mm-ss':
-        tempArray = [hou, min, sec]
-        break
-      case 'hh-mm':
-        tempArray = [hou, min]
-        break
-      default:
-        let temp = day + '\xa0\xa0\xa0' + [hou, min, sec].join(':')
-        tempArray = [yea, mon, temp]
+    case 'YY-MM-DD':
+      tempArray = [yea, mon, day]
+      break
+    case 'YY-MM':
+      tempArray = [yea, mon]
+      break
+    case 'MM-DD':
+      tempArray = [mon, day]
+      break
+    case 'hh-mm-ss':
+      tempArray = [hou, min, sec]
+      break
+    case 'hh-mm':
+      tempArray = [hou, min]
+      break
+    default:
+      let temp = day + '\xa0\xa0\xa0' + [hou, min, sec].join(':')
+      tempArray = [yea, mon, temp]
   }
   return tempArray.join(separator)
 }
