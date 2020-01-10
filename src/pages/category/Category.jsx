8933b2205fd86185d6d04cd528e49d5017d7be90
chuhx <chuhx@yonyou.com>
2020-01-10 16:47:28 +0800

    获取到一级分类

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,38 +1,14 @@
 import React, { Component } from 'react'
 import { Card, Button, Table, Divider } from 'antd'
+import { reqCategorys } from '../../api'
 
 export default class Category extends Component {
-  render() {
-    // card 的左侧
-    const title = '一级分类列表'
-    // card 的右侧按钮
-    const extra = (
-      <Button type="primary" icon="plus">
-        添加
-      </Button>
-    )
-    const dataSource = [
-      {
-        "parentId": "0",
-        "_id": "5c2ed631f352726338607046",
-        "name": "分类001",
-        "__v": 0
-      },
-      {
-        "parentId": "0",
-        "_id": "5c2ed647f352726338607047",
-        "name": "分类2",
-        "__v": 0
-      },
-      {
-        "parentId": "0",
-        "_id": "5c2ed64cf352726338607048",
-        "name": "1分类3",
-        "__v": 0
-      }
-    ];
-    
-    const columns = [
+  state = {
+    category: []
+  }
+  // 定义分类列表的子项
+  initTableheader = () => {
+    this.columns = [
       {
         title: '分类名称',
         dataIndex: 'name',
@@ -52,11 +28,38 @@ export default class Category extends Component {
         ),
       },
     ];
+  }
+  
+  // 获取分类数据
+  reqCategorys = async () => {
+    let parentId = 0
+    let res = await reqCategorys({parentId})
+    console.log(res)
+  } 
+
+  componentWillMount () {
+    this.initTableheader()
+  }
+  componentDidMount () {
+    this.reqCategorys()
+  }
+
+  render() {
+    // 读取状态数据
+    const { category } = this.state
+    // card 的左侧
+    const title = '一级分类列表'
+    // card 的右侧按钮
+    const extra = (
+      <Button type="primary" icon="plus">
+        添加
+      </Button>
+    )
     return (
       <Card title={title} extra={extra} >
         <Table 
-          dataSource={dataSource} 
-          columns={columns} 
+          dataSource={category} 
+          columns={this.columns} 
           bordered
         />
       </Card>
