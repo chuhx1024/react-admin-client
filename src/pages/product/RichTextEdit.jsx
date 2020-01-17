319293ed830fb8ca291351921a4f434b38b321cc
chuhx <chuhx@yonyou.com>
2020-01-17 14:03:17 +0800

    富文本编辑器 中的图片上传功能实现

--- src/pages/product/RichTextEdit.jsx
+++ src/pages/product/RichTextEdit.jsx
@@ -38,7 +38,29 @@ export default class RichTextEdit extends Component {
     this.setState({
       editorState,
     });
-  };
+  }
+  // 图片上传的功能
+  uploadImageCallBack = (file) => {
+    return new Promise(
+      (resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '/manage/img/upload');
+        xhr.setRequestHeader('Authorization', 'Client-ID XXXXX');
+        const data = new FormData();
+        data.append('image', file);
+        xhr.send(data);
+        xhr.addEventListener('load', () => {
+          const response = JSON.parse(xhr.responseText);
+          resolve(response)
+          // resolve({data: {link: response.data.url}})
+        });
+        xhr.addEventListener('error', () => {
+          const error = JSON.parse(xhr.responseText);
+          reject(error);
+        });
+      }
+    );
+  }
   render() {
     const { editorState } = this.state
     return (
@@ -47,6 +69,9 @@ export default class RichTextEdit extends Component {
           editorState={editorState}
           editorStyle={{border: '1px solid #000', minHight: 400, padding: '0 10px'}}
           onEditorStateChange={this.onEditorStateChange}
+          toolbar={{
+            image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: true } },
+          }}
         />
         <textarea
           disabled
